8f51a65c9d879681cb24bc5be8f658d970ed0d82
Dzmitry Navitski <dzmitry.navitski@gmail.com>
2014-08-31 17:35:03 +0300

    Rewrite plugin from scratch

--- js/dd.js
+++ js/dd.js
@@ -1,47 +1,147 @@
-(function($) {
-    $.fn.dropdown = function() {
+;
+(function($, window, document, undefined) {
 
-        return this.each(function() {
+    // undefined is used here as the undefined global variable in ECMAScript 3 is
+    // mutable (ie. it can be changed by someone else). undefined isn't really being
+    // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
+    // can no longer be modified.
 
-            var $dd = $(this);
-            var $placeholder = $dd.children('span');
-            var $input = $dd.children('input');
-            var $list = $dd.find('.dropdown');
-            var $opts = $dd.find('.dropdown > li');
-            var $first = $opts.first();
-            var $firstText = $first.text();
-            var $firstVal = $first.data('value');
+    // window and document are passed through as local variable rather than global
+    // as this (slightly) quickens the resolution process and can be more efficiently
+    // minified (especially when both are regularly referenced in your plugin).
 
-            $first.addClass('selected');
-            $placeholder.text($firstText);
-            $input.val($firstVal);
+    // Create the defaults once
+    var pluginName = "dropdown",
+        defaults = {
+            firstSelected: true,
+            input: true,
+            onInit: function() {},
+            onOpen: function() {},
+            onClose: function() {},
+            onChoose: function() {}
+        };
 
-            $(document).on('click', $dd, function(event) {
-                event.preventDefault();
-                $dd.toggleClass('active');
-            });
+    // The actual plugin constructor
+    function Plugin(element, options) {
+        //elements
+        this.$element = $(element);
+        this.nameEnabled = this.$element.data('name-enabled');
+        this.valueEnabled = this.$element.data('value-enabled');
+        this.placeholder = this.$element.find('.dropdown-placeholder');
+        this.list = this.$element.find('.dropdown');
+        this.options = this.$element.find('.dropdown-option');
 
-            $opts.on('click', function(event) {
-                event.preventDefault();
-                var $opt = $(this);
-                var $val = $opt.data('value');
-                var $text = $opt.text();
+        this.settings = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+        this.closeDropdown();
+        this.openDropdown();
+        this.chooseOption();
+    }
 
-                $placeholder.text($text);
-                $input.val($val);
+    // Avoid Plugin.prototype conflicts
+    $.extend(Plugin.prototype, {
+        init: function() {
+            // if input enabled
+            if (this.settings.input) {
+                var $inputEl = '<input class="dropdown-input" type="hidden">';
 
-                if (!$opt.hasClass('selected')) {
-                    $opts.removeClass('selected');
-                    $opt.addClass('selected');
+                this.$element.prepend($inputEl);
+                this.input = this.$element.find('.dropdown-input');
+            }
+
+            // if first option is selected by default
+            if (this.settings.firstSelected) {
+                var $firstItem = this.options.first();
+                var $firstItemText = $firstItem.text();
+
+                $firstItem.addClass('is-selected');
+                this.placeholder.text($firstItemText);
+
+                //check if input option is enabled
+                if (this.settings.input) {
+                    //if name enabled
+                    if (this.nameEnabled) {
+                        var $firstItemName = $firstItem.data('name');
+                        this.input.attr('name', $firstItemName);
+                    }
+
+                    //if value enabled
+                    if (this.valueEnabled) {
+                        var $firstItemValue = $firstItem.data('value');
+                        this.input.attr('value', $firstItemValue);
+                    }
+                }
+            }
+
+            this.settings.onInit.call(this.$element, []);
+        },
+        openDropdown: function() {
+            var $this = this;
+
+            $this.$element.click(function(event) {
+                if (!$this.$element.hasClass('is-active')) {
+                    $this.$element.addClass('is-active');
+                    $this.settings.onOpen.call($this.$element, []);
+                } else {
+                    $this.$element.removeClass('is-active');
+                    $this.settings.onClose.call($this.$element, []);
                 }
             });
+        },
+        closeDropdown: function() {
+            var $this = this;
 
             $(document).on('click', function(event) {
-                if ($(event.target).closest($dd).length === 0) {
-                    $dd.removeClass('active');
+                if ($(event.target).closest($this.$element).length === 0 && $this.$element.hasClass('is-active')) {
+                    $this.$element.removeClass('is-active');
+                    $this.settings.onClose.call($this.$element, []);
                 }
             });
+        },
+        chooseOption: function() {
+            var $this = this;
+
+            $this.options.click(function(event) {
+                event.stopPropagation();
+                var $self = $(this);
+                var $text = $self.text();
+
+                $this.$element.removeClass('is-active');
+                $this.options.removeClass('is-selected');
+                $self.addClass('is-selected');
+                $this.placeholder.text($text);
+
+                //if name enabled
+                if ($this.nameEnabled) {
+                    var $name = $self.data('name');
+                    $this.input.attr('name', $name);
+                }
+
+                //if value enabled
+                if ($this.valueEnabled) {
+                    var $value = $self.data('value');
+                    $this.input.attr('value', $value);
+                }
+
+                $this.settings.onChoose.call($self, []);
+                $this.settings.onClose.call($this.$element, []);
+            });
+        }
+    });
+
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function(options) {
+        this.each(function() {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
+            }
         });
+
+        // chain jQuery functions
+        return this;
     };
 
-}(jQuery));
+})(jQuery, window, document);
